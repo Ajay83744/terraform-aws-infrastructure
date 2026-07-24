6728e6d67ceeb30884792eaee6d8c62b294bc73f
Ajay83744 <ajayreddy83744@gmail.com>
2026-07-23 21:15:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,6 +100,16 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
+user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+
+echo "<h1>Welcome to Ajay's Terraform Web Server</h1>" > /usr/share/nginx/html/index.html
+EOF
+
   tags = {
     Name = var.instance_name
   }
