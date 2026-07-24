5c002dd8b282f078c79c4b6140bfa9091c80e061
Ajay83744 <ajayreddy83744@gmail.com>
2026-07-23 20:25:02 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
 
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 Instance Name"
+  type        = string
+}
