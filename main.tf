122011868b70e901779d182cb39d8a9b7addf2b1
Ajay83744 <ajayreddy83744@gmail.com>
2026-07-23 21:09:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,4 +92,16 @@ resource "aws_security_group" "web_sg" {
     Name = "Web-Security-Group"
   }
 }
+# Create EC2 Instance
+resource "aws_instance" "web_server" {
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.instance_name
+  }
+}
 
